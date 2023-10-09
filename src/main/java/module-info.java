eeaa043f819802a3f8bf60930ee103adee011bff
PipiPupu <98098760+BonaNoel@users.noreply.github.com>
2023-10-09 16:30:56 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module hu.unideb.inf.sfm2023h10.cs3.bikerevolution {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens hu.unideb.inf.sfm2023h10.cs3.bikerevolution to javafx.fxml;
-    exports hu.unideb.inf.sfm2023h10.cs3.bikerevolution;
-}
