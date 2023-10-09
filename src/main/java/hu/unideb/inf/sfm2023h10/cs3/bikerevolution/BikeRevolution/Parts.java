2b4fb5994794aa21facfcfddae92ee6ee70db15a
PipiPupu <bonanoel1021@gmail.com>
2023-10-09 22:15:27 +0200

    Commit main changes to my branch

---
+++ src/main/java/hu/unideb/inf/sfm2023h10/cs3/bikerevolution/BikeRevolution/Parts.java
@@ -0,0 +1,20 @@
+package hu.unideb.inf.sfm2023h10.cs3.bikerevolution.BikeRevolution;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Parts {
+    @Id
+    @GeneratedValue
+    private int ID;
+    private int Belso;
+    private String Torzsvendeg;
+}
