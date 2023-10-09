03bb9c2b973004159f8577371788659475077dbf
Lengyel Szilárd <lenszu@mailbox.unideb.hu>
2023-10-09 19:36:09 +0200

    githab,first commit

--- src/main/java/hu/unideb/inf/sfm2023h10/cs3/bikerevolution/BikeRevolution/BikeRevolutionApplication.java
+++ src/main/java/hu/unideb/inf/sfm2023h10/cs3/bikerevolution/BikeRevolution/BikeRevolutionApplication.java
@@ -10,4 +10,6 @@ public class BikeRevolutionApplication {
 		SpringApplication.run(BikeRevolutionApplication.class, args);
 	}
 
+	
+
 }
---
+++ src/test/java/Parts.java
@@ -0,0 +1,11 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+
+public class Parts {
+    private int Belso;
+    private String Torzsvendeg;
+}
