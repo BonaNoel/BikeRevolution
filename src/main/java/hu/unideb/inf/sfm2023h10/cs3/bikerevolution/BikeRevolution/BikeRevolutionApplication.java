5d6fe12732486e732f22c73d925973e8206ae2fd
BonaNoel <bonanoel1021@gmail.com>
2023-10-09 16:34:38 +0200

    Project Start with Spring Boot

---
+++ src/main/java/hu/unideb/inf/sfm2023h10/cs3/bikerevolution/BikeRevolution/BikeRevolutionApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.sfm2023h10.cs3.bikerevolution.BikeRevolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRevolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeRevolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/inf/sfm2023h10/cs3/bikerevolution/BikeRevolution/BikeRevolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.sfm2023h10.cs3.bikerevolution.BikeRevolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRevolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
