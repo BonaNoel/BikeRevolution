5b3036bc54e395103512a3226c2d518f8ac90800
PipiPupu <bonanoel1021@gmail.com>
2023-11-12 13:26:57 +0100

    small changes

--- src/main/java/com/sfm2023/BikeRevolution/FXMLController.java
+++ src/main/java/com/sfm2023/BikeRevolution/FXMLController.java
@@ -1,27 +1,57 @@
 package com.sfm2023.BikeRevolution;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FXMLController implements Initializable {
 
+    @FXML
+    private TextField arTextField;
 
     @FXML
-    private Label label;
+    private Tab foglalasTab;
 
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private TreeView<?> foglalasokTreeView;
 
+    @FXML
+    private TextField nevTextField;
+
+    @FXML
+    private ListView<?> raktarListVieW;
+
+    @FXML
+    private Tab raktarTab;
+
+    @FXML
+    private Tab szervizFoglalasTab;
+
+    @FXML
+    private ChoiceBox<?> szervizTipusChoiceBox;
+
+    @FXML
+    private TabPane tabController;
+
+    @FXML
+    private TextField telefonTextField;
+
+    @FXML
+    private Tab webTab;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
+
+
     }
-}
+
+
+
